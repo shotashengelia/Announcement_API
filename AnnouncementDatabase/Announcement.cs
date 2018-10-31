bf5549b083f109e7a25b04c13a1cc9156a36be37
shota <shota.shengelia06@geolab.edu.ge>
2018-10-31 14:17:19 +0400

    Add project files.

---
+++ AnnouncementDatabase/Announcement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AnnouncementDatabase
+{
+    class Announcement
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(300)]
+        [Required]
+        public string Description { get; set; }
+
+        [MaxLength(32)]
+        public string ImageName { get; set; }
+
+        [MaxLength(20)]
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ AnnouncementDatabase/Context/AnnouncementDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnnouncementDatabase.Context
+{
+    public class AnnouncementDbContext : DbContext
+    {
+        public AnnouncementDbContext(DbContextOptions<AnnouncementDbContext> options) : base(options) { }
+        
+        private DbSet<Announcement> Announcements { get; set; }             //must be public!!! 
+
+    }
+}
